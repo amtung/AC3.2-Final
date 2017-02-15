ec0abdc430b96adcc320a48b5cf525af584e2267
Annie <annie.mmt@gmail.com>
2017-02-15 13:08:11 -0500

    log in screen complete

---
+++ AC3.2-Final/AC3.2-Final/LoginViewController.swift
@@ -0,0 +1,123 @@
+//
+//  LoginViewController.swift
+//  
+//
+//  Created by Annie Tung on 2/15/17.
+//
+//
+
+import UIKit
+import FirebaseAuth
+
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: - Properties
+
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    var activeTextField: UITextField?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        emailField.delegate = self
+        passwordField.delegate = self
+
+        let _ = FIRAuth.auth()?.addStateDidChangeListener({ (auth, user) in
+            self.updateInterface()
+        })
+    }
+
+    // MARK: - Methods and Actions
+    
+    func updateInterface() {
+        if let _ = FIRAuth.auth()?.currentUser {
+//            self.performSegue(withIdentifier: "tabBarSegue", sender: self)
+            loginButton.setTitle("Sign Out", for: .normal)
+        } else {
+            loginButton.setTitle("Sign In", for: .normal)
+        }
+    }
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if FIRAuth.auth()?.currentUser != nil {
+            do {
+                try FIRAuth.auth()?.signOut()
+            } catch {
+                print("Error loggin in: \(error.localizedDescription)")
+            }
+        }
+        else if let email = emailField.text,
+            let password = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                
+                if user != nil {
+                    self.performSegue(withIdentifier: "tabBarSegue", sender: sender)
+git 
+                    let alert = UIAlertController(title: "Login Successful.", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+                    let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                    alert.addAction(ok)
+                    self.present(alert, animated: true, completion: nil)
+                }
+                else {
+                    let alert = UIAlertController(title: "Login failed!", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                    let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                    alert.addAction(ok)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            })
+        }
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        print("this works")
+        if let email = emailField.text,
+            let password = passwordField.text {
+            FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                if user != nil {
+                    print("User can register")
+                    let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+                    let loginViewController = storyBoard.instantiateViewController(withIdentifier: "MainFeed") as! MainFeedTableViewController
+                    self.present(loginViewController, animated:true, completion:nil)
+                }
+                else {
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
+                    let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                    alert.addAction(ok)
+                    self.present(alert, animated: true, completion: nil)
+                }
+            })
+        }
+    }
+    
+    // MARK: - UITextFieldDelegate
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeTextField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        activeTextField = nil 
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.passwordField {
+            view.endEditing(true)
+            return false
+        }
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AC3.2-Final/AC3.2-Final/MainFeedTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MainFeedTableViewCell.swift
+//  
+//
+//  Created by Annie Tung on 2/15/17.
+//
+//
+
+import UIKit
+import SnapKit
+
+class MainFeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        
+    
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+
+}
